26308e9aea8a08e2ec20114d3350be7218b7cdfa
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-05-31 21:45:43 +0900

    update file

--- src/routes/resume.router.js
+++ src/routes/resume.router.js
@@ -108,35 +108,5 @@ router.put("/resume/:resumeid", authMiddlware, async (req, res, next) => {
     next(err);
   }
 });
-//관리자 패널=======================================
-
-//관리자의 모든 이력서 상세조회 router
-router.get("/manager/resume/:resumeid", authMiddlware, async(req,res,next) => {
-    //사용자가 매니저인지 확인
-    if (!req.manager) {
-        const error= new Error(`사이트에 접근할 권한이 없습니다!`);
-        error.name='ForbiddenError';
-        return next(error);
-    }
-
-    const user = req.user;
-    const resumes=await prisma.resume.findMany();
-
-
-    return res.status(200).json({resumes});
-});
-
-//관리자의 이력서 수정 router
-router.put("/manager/resume/:resumeid", authMiddlware, (req,res,next) => {
-    //사용자가 매니저인지 확인
-    if (!req.manager) {
-        const error= new Error(`사이트에 접근할 권한이 없습니다!`);
-        error.name='ForbiddenError';
-        return next(error);
-    }
-    const user = req.user;
-
-    return res.status(200).json({Message:"코드 검증 완료"});
-});
 
 export default router;
